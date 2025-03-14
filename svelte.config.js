3b2298eeb6ea8c514f5b28c73b283125bb26088d
Gergely Bodor <bgolyoo@gmail.com>
2025-03-14 13:48:25 +0100

    chore: build with cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,13 +12,15 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      // pages: 'build',
-      // assets: 'build',
-      fallback: 'index.html'
-      // precompress: false,
-      // strict: true
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      },
+      platformProxy: {
+        configPath: undefined,
+        environment: undefined,
+        persist: undefined
+      }
     })
   }
 };
